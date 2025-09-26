26df173d727041652c006b135ca9d0122c83e6dc
Deepak kumar <deepakkumar@Deepaks-MacBook-Air.local>
2025-09-26 08:40:25 +0530

    Initial commit

---
+++ src/view-trip/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <h2 className='text-center text-gray-400'>Created with React</h2>
+    </div>
+  )
+}
+
+export default Footer
